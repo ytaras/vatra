ade14312a9ee47c0b70c9f33e687eb40f0cb5890
Yura Taras <ytaras@softserveinc.com>
2015-03-26 20:08:16 +0200

    Side wind adjustment

--- src/js/services/data.js
+++ src/js/services/data.js
@@ -9,12 +9,24 @@ angular.module('Vatra.services.Data', ['Vatra.services.HardcodedTables'])
             }
             return lookupByDistance(dictionary, data.distance)
         }
-    }).factory('sightsValues', function (sightsTable, derivationAdjustments, lookupByDistance) {
+    }).factory('sightsValues', function (sightsTable, derivationAdjustments, clockToRadian, sideWindAdjustment, lookupByDistance) {
         return function (data) {
+            var windDirection = clockToRadian(data.windDirection);
+            var forwardWind = Math.cos(windDirection) * data.windSpeed;
+            var sideWind = Math.sin(windDirection) * data.windSpeed;
             return {
                 originalSights: lookupByDistance(sightsTable[data.trajectory], data.distance),
-                derivationAdjustment: lookupByDistance(derivationAdjustments[data.trajectory], data.distance)
+                derivationAdjustment: lookupByDistance(derivationAdjustments[data.trajectory], data.distance),
+                sideWindAdjustment: lookupByDistance(sideWindAdjustment[data.trajectory], data.distance) * sideWind / 10,
+                windInRadians: windDirection,
+                forwardWind: forwardWind,
+                sideWind: sideWind
             }
+        }
+    }).factory('clockToRadian', function () {
+        return function (clock) {
+            var degrees = (clock % 12) * 30;
+            return degrees * (Math.PI / 180);
         }
     }).factory('lookupByDistance', function () {
         return function (table, distance) {
